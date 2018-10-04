50ca4f2a0b6e4ff381f7d7709dba1b6910a85f10
Yeidy Yannelli <yeidyyannelli@D341428.mdc.edu>
2018-10-04 19:18:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/yeidyyannelli/firstgithub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yeidyyannelli.firstgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
